e05efd9f7e99d4ee112f0b5ea1d93b5bcdd2c33b
Braddinger13 <78782966+Braddinger13@users.noreply.github.com>
2022-07-26 19:44:35 -0400

    Backend

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 //Creates connection
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Owltime13!",
-//   database: "nodejsdemo",
-// });
+const db = mysql.createConnection({
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b5afa61b88c1ab",
+  password: "1c788992",
+  database: "heroku_43746ee8f304c3f",
+});
 
-// db.connect( (err) => {
-//     if(err){
-//         throw err;
-//     }
-//     console.log('MySql connected...');
-// });
+db.connect( (err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql connected...');
+});
 
 
 //get all user api
 app.get("/api/listAllUsers", (req, res) => {
-    const sqlInsert = "SELECT * FROM nodejsdemo.web_user;";
+    const sqlInsert = "SELECT * FROM web_user;";
     db.query(sqlInsert, (req, result) => {
         res.send(result);
     });
